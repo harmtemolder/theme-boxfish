6851db26b101207c12efdbcfd1dfe5a1d3db4bfd
Harm <mail@harmtemolder.com>
2020-09-24 11:37:36 -0300

    Add iTerm2 prompt mark

--- fish_mode_prompt.fish
+++ fish_mode_prompt.fish
@@ -1,4 +1,10 @@
 function fish_mode_prompt -d "Prefix a single, colored block indicating vi mode"
+  # Add iTerm2 prompt mark (see https://www.iterm2.com/documentation-shell-integration.html)
+  if functions -q iterm2_prompt_mark
+    iterm2_prompt_mark
+  end
+
+  # Prefix a single, colored block indicating vi mode
   if [ $fish_key_bindings = fish_vi_key_bindings ]
     switch $fish_bind_mode
       case default
